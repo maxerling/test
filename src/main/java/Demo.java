8528aa0d4a4e382c399015555a00eafecfffcc00
maxerling <maximilianerling@gmail.com>
2020-10-21 23:37:04 +0200

    added comment

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -40,6 +40,7 @@ public class Demo extends JFrame {
         mainPanel.add(checkBoxPanel);
         mainPanel.add(displayArea);
         //more comments
+        //ad
         checkBoxPanel.add(red);
         checkBoxPanel.add(yellow);
         checkBoxPanel.add(blue);
